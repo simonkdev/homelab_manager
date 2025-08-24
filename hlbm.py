885106983eba167b5043f95dd8afae84036bb188
simonkdev <simonkorten09@gmail.com>
2025-08-24 16:05:40 +0200

    addded get_device_info

--- hlbm.py
+++ hlbm.py
@@ -6,7 +6,7 @@ def get_devices():
     """
     A method that returns an array filled with the devices 
     present in the csv file. Each Object within the array
-    is a dict with the keys IP (ip address), 
+    is a dict with the keys NAME (name of device), IP (ip address), 
     UNAME (username) and PASSWD (password).
     """
     file = open('devices.csv', 'r') 
@@ -24,4 +24,25 @@ def get_devices():
         targets.append(obj)
     return(targets)
 
-print(get_devices())
+def get_device_info(name, key):
+    devices = get_devices
+    for device in devices:
+        if device.NAME == name:
+            if key == "NAME" or key == "name":
+                return device.NAME
+            if key == "IP" or key == "ip":
+                return device.IP
+            if key == "UNAME" or key == "uname":
+                return device.UNAME
+            if key == "PASSWD" or key == "passwd" or key == "psk":
+                return device.PASSWD
+            else:
+                raise ValueError("Please use a valid key for method get_device_info!")
+    raise ValueError("Specified device was not found!")
+
+def add_device():
+    """
+    A method that lets the user add a device to the csv file 
+    (devices.csv) via an interactive shell.
+    """
+    
